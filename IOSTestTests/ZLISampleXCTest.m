2d8c16f2b6aaabf15a7552c7fb83d6abde51fe45
Zhou Li <zhoulijoe@gmail.com>
2015-06-01 23:02:39 -0700

    Move more test to objc basic project

--- IOSTestTests/ZLISampleXCTest.m
+++ IOSTestTests/ZLISampleXCTest.m
@@ -1,44 +1,10 @@
 #import <XCTest/XCTest.h>
-#import "ZLISuperClass.h"
 
 @interface ZLISampleXCTest : XCTestCase
-
-@property ZLISuperClass *sc;
 
 @end
 
 @implementation ZLISampleXCTest
-
-+ (void)setUp {
-    NSLog(@"Test class setup: %@", NSStringFromSelector(_cmd));
-}
-
-+ (void)tearDown {
-    NSLog(@"Test class tearDown: %@", NSStringFromSelector(_cmd));
-}
-
-- (void)setUp {
-    NSLog(@"%@ per test setup", NSStringFromSelector(_cmd));
-    [super setUp];
-
-    self.sc = [ZLISuperClass new];
-}
-
-- (void)tearDown {
-    NSLog(@"%@ per test tearDown", NSStringFromSelector(_cmd));
-    [super tearDown];
-}
-
-/**
- * Dummy test for method that returns a string
- */
-- (void)testGreeting {
-    NSString *input = @"hello";
-    NSString *expectedOutput = @"hello";
-    NSString *output = [self.sc saySomething:input];
-    XCTAssertEqualObjects(output, expectedOutput, @"ZLISuperClass saySomething with input %@ "
-                          @"outputs %@ instead of %@", input, output, expectedOutput);
-}
 
 /**
  * Test case have access to the application
